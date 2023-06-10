e745ac0f32bd9fa67302a8a9b868e1006dd3e955
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-06-10 18:09:35 +0900

    feat: Error 타입에 Description 추가

--- Diary/Source/Common/DiaryError.swift
+++ Diary/Source/Common/DiaryError.swift
@@ -1,7 +1,20 @@
 //  Diary - DiaryManager.swift
 //  Created by Ayaan, zhilly on 2023/01/02.
 
+import Foundation
+
 enum DiaryError: Error {
     case failedFetchEntity
     case invalidObjectID
+}
+
+extension DiaryError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .failedFetchEntity:
+            return "다이어리 가져오기 실패"
+        case .invalidObjectID:
+            return "잘못된 다이어리 ID"
+        }
+    }
 }
