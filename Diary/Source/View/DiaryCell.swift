db6dafcba6dba8eeeb6ac6357faf392478d95334
Zhilly <99257965+zhilly11@users.noreply.github.com>
2023-06-10 18:12:43 +0900

    feat: Cell에 Weather Image 추가

--- Diary/Source/View/DiaryCell.swift
+++ Diary/Source/View/DiaryCell.swift
@@ -44,6 +44,14 @@ final class DiaryCell: UITableViewCell, ReusableView {
         return stackView
     }()
     
+    private let weatherIconImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        
+        return imageView
+    }()
+    
     private let dateAndBodyStackView: UIStackView = {
         let stackView = UIStackView()
         
@@ -65,22 +73,29 @@ final class DiaryCell: UITableViewCell, ReusableView {
     }
     
     private func setupViews() {
-        dateAndBodyStackView.addArrangedSubview(createdDateLabel)
-        dateAndBodyStackView.addArrangedSubview(bodyLabel)
-        contentsStackView.addArrangedSubview(titleLabel)
-        contentsStackView.addArrangedSubview(dateAndBodyStackView)
-        contentView.addSubview(contentsStackView)
+        [createdDateLabel,
+         weatherIconImageView,
+         bodyLabel].forEach(dateAndBodyStackView.addArrangedSubview(_:))
+        
+        [titleLabel,
+         dateAndBodyStackView].forEach(contentsStackView.addArrangedSubview(_:))
+        
+        [contentsStackView].forEach(contentView.addSubview(_:))
         
         let contentsStackViewBottomConstraint = contentsStackView.bottomAnchor.constraint(
             equalTo: contentView.bottomAnchor,
-            constant: -8)
+            constant: -8
+        )
         contentsStackViewBottomConstraint.priority = .defaultHigh
         
         NSLayoutConstraint.activate([
             contentsStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
             contentsStackViewBottomConstraint,
             contentsStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
-            contentsStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8)
+            contentsStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            
+            weatherIconImageView.widthAnchor.constraint(equalToConstant: 24),
+            weatherIconImageView.heightAnchor.constraint(equalToConstant: 24)
         ])
         
         createdDateLabel.setContentHuggingPriority(.required, for: .horizontal)
@@ -92,5 +107,16 @@ final class DiaryCell: UITableViewCell, ReusableView {
         titleLabel.text = cellContents.title
         bodyLabel.text = cellContents.body
         createdDateLabel.text = cellContents.createdAt
+        loadWeatherIcon(id: diary.weatherIconID)
+    }
+    
+    private func loadWeatherIcon(id: String?) {
+        let networkManager = NetworkManager()
+        guard let id = id else { return }
+        
+        Task.init {
+            let iconImageData = try await networkManager.fetchWeatherIcon(id: id)
+            self.weatherIconImageView.image = UIImage(data: iconImageData)
+        }
     }
 }
